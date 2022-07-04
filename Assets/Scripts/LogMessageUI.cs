ac09bef2b1f93db162c69a96db9227201218594e
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-07-04 18:32:45 +0900

    :recycle:ログをオブジェクトで表示 #4

---
+++ Assets/Scripts/LogMessageUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LogMessageUI : MonoBehaviour
+{
+    TextMeshPro UIText = null;
+
+    string messages = "";
+
+    void Start()
+    {
+        UIText = GameObject.Find("LogMessageText").GetComponent<TextMeshPro>();
+
+        Application.logMessageReceived += logMessageReceived;
+    }
+
+    private void logMessageReceived(string condition, string stackTrace, LogType type)
+    {
+        messages = condition + "\n" + messages;
+
+        UIText.text = messages;
+
+        switch (type)
+        {
+            case LogType.Error:
+                break;
+            case LogType.Assert:
+                break;
+            case LogType.Warning:
+                break;
+            case LogType.Log:
+                break;
+            case LogType.Exception:
+                break;
+        }
+    }
+}
