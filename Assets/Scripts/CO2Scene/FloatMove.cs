572e557d434d7ff75e72d082c04380370350fae4
TbrtTkyk <103414431+TbrtTkyk@users.noreply.github.com>
2022-07-12 13:42:43 +0900

    移動の詳細をインスペクタから設定できるようにした

--- Assets/Scripts/CO2Scene/FloatMove.cs
+++ Assets/Scripts/CO2Scene/FloatMove.cs
@@ -5,38 +5,59 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class FloatMove : MonoBehaviour
 {
+    // 加える力の大きさの範囲設定
+    [SerializeField] public float minForcePower = 0.1f;
+    [SerializeField] public float maxForcePower = 1f;
+    // 力を加える間隔の範囲設定
+    [SerializeField] public float minMoveDuration = 0.2f;
+    [SerializeField] public float maxMoveDuration = 3f;
+
+    // 移動の継続フラグ
     private bool isFloating = true;
 
+    // Rigidbodyのキャッシュ
     private Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        // Start()と同時に移動を開始する
         StartCoroutine(Floating());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     // 数秒おきにオブジェクトに対して力を加える
     private IEnumerator Floating() {
+        isFloating = true;
         while (true) {
+            // isFloatingがFalseなら力を加えるのを止める
+            if (!isFloating)
+            {
+                yield break;
+            }
             // ランダムな方向へランダムな大きさの力を加える
-            float Length = Random.Range(0.1f, 1f);
+            float Length = Random.Range(minForcePower, maxForcePower);
             Vector3 force = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f)) * Vector3.up;
             _rb.AddForce(force * Length, ForceMode.Impulse);
             _rb.AddTorque(force, ForceMode.Impulse);
-            // isFloatingがFalseなら力を加えるのを止める
-            if (!isFloating) {
-                yield break;
-            }
             // １～３秒程度待機する
-            float waitTime = Random.Range(0.2f, 3f);
+            float waitTime = Random.Range(minMoveDuration, maxMoveDuration);
             yield return new WaitForSeconds(waitTime);
         }
+    }
+
+    // 移動の開始
+    public void startFloat()
+    {
+        if( !isFloating )
+        {
+            StartCoroutine(Floating());
+        }
+    }
+
+    // 移動の終了
+    public void endFloat()
+    {
+        isFloating = false;
     }
 }
