76bf31762b1db0deef3fe6e64324b5af43381e15
TbrtTkyk <103414431+TbrtTkyk@users.noreply.github.com>
2022-08-07 18:48:35 +0900

    スポナーに最小生成数を設定できるようにした

--- Assets/Scripts/CO2Scene/Spawner.cs
+++ Assets/Scripts/CO2Scene/Spawner.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject prefab;
-    [SerializeField] private int MaxNum = 30;
-    [SerializeField] private int nowNum = 0;
+    [SerializeField] private GameObject prefab; // 生成するオブジェクト（Prefab）
+    [SerializeField] private int MinNum = 3;    // オブジェクトの最小生成数
+    [SerializeField] private int MaxNum = 30;   // オブジェクトの最大生成数
+    public int nowNum = 0;                      // 現在のオブジェクトの設定数
 
-    private List<GameObject> objects = new List<GameObject>();
-    private bool isSpawnable => objects.Count < MaxNum;
+    private List<GameObject> objects = new List<GameObject>();  // 管理しているオブジェクトのリスト
+    private bool isSpawnable => objects.Count < MaxNum; // 生成可能かのフラグ
 
-    public int Num => objects.Count;
+    public int Num => objects.Count;    // 現在管理しているオブジェクトの数
+
+    private void Start()
+    {
+        if (MinNum > 0) Add(MinNum);
+    }
 
     private void Update()
     {
+        // オブジェクトの設定数と管理数に差がある場合、その分だけオブジェクトを追加・削除する
         if( nowNum < Num ) {
             Sub(Num - nowNum);
         }
@@ -24,17 +31,23 @@ public class Spawner : MonoBehaviour
         }
     }
 
+    // 数を指定して、その分オブジェクトを増やす
     private void Add(int n = 1)
     {
-        if (n <= 0 || n + Num > MaxNum) return;
+        if (n <= 0 || Num >= MaxNum) return;
+        else if (n + Num >= MaxNum) n = MaxNum - Num;
         for(int i = 0; i < n; i++)
         {
             CreateObject();
         }
     }
+    // 数を指定して、その分オブジェクトを減らす
     private void Sub(int n = 0)
     {
-        if (n <= 0 || n > Num) return;
+        if (n <= 0) return;
+        else if (Num - n < MinNum) n = Num - MinNum;
+
+        if (n > Num) n = Num;
         for(int i = 0; i < n; i++)
         {
             DestroyObject(objects[0]);
