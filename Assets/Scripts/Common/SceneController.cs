c0c7caef755fb841d8cda91949a6053d22fa110b
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-08-16 11:41:29 +0900

    SceneController スクリプトを作成

---
+++ Assets/Scripts/Common/SceneController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void TransitionToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
