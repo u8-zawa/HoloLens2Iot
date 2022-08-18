970e3a78e24ce747ed0d729c567ec9aea4c8121e
kotaro210 <kotaros@kotaronoMacBook-Air.local>
2022-08-19 00:56:22 +0900

    閾値以上か以下かを判定する機能を追加

---
+++ Assets/Scripts/UDScene/ThresholdJudge.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThresholdJudge : MonoBehaviour
+{
+    [SerializeField] private int threshold = 200;    // 閾値 (単位:cm)
+    public int nowValue = 0;    //　現在の超音波距離センサーの測定値
+    public bool IsOver { get; private set; }    // 現在のセンサーの測定値が閾値以上か以下かのフラグ
+    
+    private void Update()
+    {
+        Judge();
+    }
+    
+    // センサーの測定値と閾値の大小判定
+    public void Judge()
+    {   
+        // センサーの測定値を取得
+        SensorDataManager sdm = SensorDataManager.Instance;
+        if (sdm == null || sdm.GetSensorData("ud") == null)
+        {
+            return;
+        }
+
+        // 現在のセンサーの測定値
+        int nowValue = sdm.GetSensorData("ud").stat.latest;
+        
+        // 現在のセンサーの測定値と閾値の大小を比較
+        if (nowValue < threshold) {
+            IsOver = false;
+            Debug.Log("less than 2m");
+        }
+        else {
+            IsOver = true;
+            Debug.Log("greater than 2m");
+        }
+    }
+}
+        
